f6077b42ba5d55df7d9a27f10bf223bd9a418b33
LWH <Luwenhai@iclooud.com>
2023-08-31 13:56:29 +0800

    fixbug

--- Project/Game/States/StateHandle/LandingState.cs
+++ Project/Game/States/StateHandle/LandingState.cs
@@ -21,24 +21,28 @@ namespace Demo
         {
             if (block.Shape == BlockShape.None)
                 return;
-            
+
             //横向和纵向没有可消除的相邻block
-            var selfController = _controller as SelfGameController;
-            var sameBlocks = selfController.GetSameBlocksWith(block);
-            if (sameBlocks.Count < 3)
+            // var selfController = _controller as SelfGameController;
+            // var sameBlocks = selfController.GetSameBlocksWith(block);
+
+            block._Animation.PlayAnimation(string.Format("{0}_{1}", block.Shape, block.State));
+            block.CurStateFrame = ConstValues.landingFps;
+
+            var otherBlock = OtherGameController.Inst.blockMatrix[block.Row, block.Col - 1];
+            otherBlock._Animation.PlayAnimation(string.Format("{0}_{1}", block.Shape, block.State));
+
+            timerID = TimerMgr._Instance.Schedule(() =>
             {
-                block._Animation.PlayAnimation(string.Format("{0}_{1}",block.Shape,block.State));
-                block.CurStateFrame = ConstValues.landingFps;
-                
-                var otherBlock = OtherGameController.Inst.blockMatrix[block.Row, block.Col - 1];
-                otherBlock._Animation.PlayAnimation(string.Format("{0}_{1}",block.Shape,block.State));
-                
-                timerID = TimerMgr._Instance.Schedule(() =>
+                block._Animation.StopAnimation(() => { block.ResetOriginImg(); });
+                otherBlock = OtherGameController.Inst.blockMatrix[block.Row, block.Col - 1];
+                otherBlock._Animation.StopAnimation(() => { otherBlock.ResetOriginImg(); });
+
+                var selfController = _controller as SelfGameController;
+                var sameBlocks = selfController.GetSameBlocksWith(block);
+
+                if (sameBlocks.Count < 3)
                 {
-                    block._Animation.StopAnimation(()=>{block.ResetOriginImg();});
-                    otherBlock = OtherGameController.Inst.blockMatrix[block.Row, block.Col - 1];
-                    otherBlock._Animation.StopAnimation(()=>{otherBlock.ResetOriginImg();});
-                    
                     var downBlock = (_controller as SelfGameController).blockMatrix[block.Row - 1, block.Col - 1];
 
                     if (downBlock.State == BlockState.Matched || downBlock.Chain)
@@ -49,18 +53,15 @@ namespace Demo
                     {
                         block.Chain = false;
                     }
-                    
-                    
-                    StateManger._instance.ChangeState(BlockState.Normal, block);
 
-                }, ConstValues.landingFps * ConstValues.fpsTime);
-            }
-            else
-            {
-                if(!selfController.IsBlockInSameFrame(block))
-                    selfController.BlocksInSameFrame.Add(block);
-            }
-            
+                    StateManger._instance.ChangeState(BlockState.Normal, block);
+                }
+                else
+                {
+                    if (!selfController.IsBlockInSameFrame(block))
+                        selfController.BlocksInSameFrame.Add(block);
+                }
+            }, ConstValues.landingFps * ConstValues.fpsTime);
         }
     }
 }
